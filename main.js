e9011ffc76089b738256e5e3c0c0a8df7cb2d7aa
sajwan05 <surisajwan1998@gmail.com>
2025-07-24 15:50:35 +0000

    MAKE RANDOM COMPUTER CHOICE Made a function which return rock, paper ,scissors randomly

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("hey");
+
+function getComputerChoice () {
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    if (computerChoice === 1) {
+        return "Rock";
+    } else if ( computerChoice === 2){
+        return "Paper";
+    }else {
+        return "Scissors";
+    }
+}
+
+
+console.log(getComputerChoice());
