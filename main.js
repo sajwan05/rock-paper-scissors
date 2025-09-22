ab979bb34ba629e2eb4d0da26b934a49e2bf27f8
sajwan05 <surisajwan1998@gmail.com>
2025-09-22 08:32:59 +0000

    Make human choice based on clicks So we made human choice based on click and event bubbling and clean up code

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-console.log(`Rock Paper Scissors Shoot! 
-    The game will go on for 5 rounds whoever have maximum  points after 5 rounds wins.
-    You get 1 point for winning round.
-    If you lose computer get 1 point,
-    For Tie you get 1 point each:
-    Let's Play: `);
+// console.log(`Rock Paper Scissors Shoot! 
+//     The game will go on for 5 rounds whoever have maximum  points after 5 rounds wins.
+//     You get 1 point for winning round.
+//     If you lose computer get 1 point,
+//     For Tie you get 1 point each:
+//     Let's Play: `);
 
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice;
+const result = document.querySelector(".result");
+const buttons = document.querySelector(".buttons");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const human = document.querySelector(".human .who");
+const bot = document.querySelector(".computer .who");
 
 function getComputerChoice () {
     let computerChoice = Math.random();
@@ -21,20 +29,18 @@ function getComputerChoice () {
     
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt(`Rock Paper scissors shoot! 
-        Please choose :
-        rock
-        paper
-        or
-        scissors`).toLowerCase();
 
-    return humanChoice;
-}
+buttons.addEventListener("click", (event) => {
+    let target = event.target;
+    humanChoice = target.id;
+    botChoice = getComputerChoice();
+
+    playRound(humanChoice, botChoice);
+});
+
 
 
 function playRound (humanChoice, computerChoice){
-    // Computer wins if computer choose paper human choose rock
     
     if (humanChoice === computerChoice) {
         humanScore++;
@@ -62,41 +68,41 @@ function playRound (humanChoice, computerChoice){
 
 
 
-function playGame () {
+// function playGame () {
 
-    for (i = 1; i <= 5; i++){
+//     for (i = 1; i <= 5; i++){
 
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        console.log(playRound(humanSelection, computerSelection));
-    }
+//         let humanSelection = getHumanChoice();
+//         let computerSelection = getComputerChoice();
+//         console.log(playRound(humanSelection, computerSelection));
+//     }
 
-    if(humanScore > computerScore){
+//     if(humanScore > computerScore){
 
-        console.log(`You won the game!Let's goo!🎉🎉
+//         console.log(`You won the game!Let's goo!🎉🎉
             
-            Final Score: You: ${humanScore} --- Bot: ${computerScore}`);
+//             Final Score: You: ${humanScore} --- Bot: ${computerScore}`);
 
-    }else if(humanScore === computerScore) {
+//     }else if(humanScore === computerScore) {
 
-        console.log(`Game is Tied! Woahh soo close!
+//         console.log(`Game is Tied! Woahh soo close!
             
-            Final Score: You: ${humanScore} --- Bot: ${computerScore}`);
+//             Final Score: You: ${humanScore} --- Bot: ${computerScore}`);
 
-    }else {
+//     }else {
 
-        console.log(`You lose! the game! Sad!!! 😭😭
+//         console.log(`You lose! the game! Sad!!! 😭😭
             
-            Final Score: You: ${humanScore} --- Bot: ${computerScore}`);
-    }
+//             Final Score: You: ${humanScore} --- Bot: ${computerScore}`);
+//     }
 
 
-    console.log(`Thanks for playing!!`);
+//     console.log(`Thanks for playing!!`);
 
 
-}
+// }
 
-playGame();
+// playGame();
 
 
 
